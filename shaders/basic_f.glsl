eec0a6961d0b49cf4c068c8f6b136bf9f83bccc5
longwatermelon <longwatermelon1@gmail.com>
2022-07-20 23:06:35 -0700

    diffuse and specular lighting

--- shaders/basic_f.glsl
+++ shaders/basic_f.glsl
@@ -4,15 +4,32 @@ out vec4 FragColor;
 in vec3 f_pos;
 in vec3 f_norm;
 in vec3 f_col;
+in vec2 f_tcoords;
 
 uniform vec3 cam_pos;
 uniform samplerCube skybox;
 
 void main()
 {
-    float ratio = 1. / 1.52;
+    vec3 ambient = .2 * f_col;
+
+    vec3 norm = normalize(f_norm);
+
+    vec3 ldir = normalize(vec3(-1., .4, .3));
+    float diff = max(dot(norm, ldir), 0.);
+    vec3 diffuse = .4 * diff * f_col;
+
+    vec3 view_dir = normalize(cam_pos - f_pos);
+    vec3 reflect_dir = reflect(-ldir, norm);
+    float spec = pow(max(dot(view_dir, reflect_dir), 0.), 100.f);
+    vec3 specular = 1. * spec * f_col;
+
+    vec3 result = ambient + diffuse + specular;
+
+    float ratio = 1. / 1.4;
     vec3 I = normalize(f_pos - cam_pos);
     vec3 R = refract(I, normalize(f_norm), ratio);
-    FragColor = vec4(mix(texture(skybox, R).rgb, f_col, .2), 1.);
+
+    FragColor = vec4(mix(texture(skybox, R).rgb, result, .3), 1.);
 }
 
